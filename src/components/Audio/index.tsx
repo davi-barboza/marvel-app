b594b33455b7971608f2ee40ce348d4856f15ce2
Davi Barboza <davicarlos_@hotmail.com>
2021-04-09 17:32:39 -0300

    audio e controle de volume funcionando com erros

--- src/components/Audio/index.tsx
+++ src/components/Audio/index.tsx
@@ -1,24 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from './styles';
 import audioIcon from '../../assets/audioIcon.png';
+import audioMuteIcon from '../../assets/audioMuteIcon.png';
 import themeMain from "../../assets/audio/themeMain.mp3";
 
 const Audio: React.FC = () => {
-  const [volume, setVolume] = useState<number>();
-  
+  const [volume, setVolume] = useState(undefined);
+  const [muted, setMuted] = useState('block');
+  const [notMuted, setNotMuted] = useState('none');
+
   useEffect(() => {
     const tagAudio = document.querySelector('.tagAudio');
     volume === undefined ? tagAudio.volume = 0.5 : tagAudio.volume = volume / 100;
   }, [volume])
 
+  function toogleMute(isMuted: string) {
+    const tagAudio = document.querySelector('.tagAudio');
+
+    if (isMuted == "muted") {
+      setMuted('none');
+      setNotMuted('block');
+
+      tagAudio.volume = 0.5;     
+      tagAudio.muted = false;
+      tagAudio.play();
+      
+    } else {
+      setMuted('block');
+      setNotMuted('none');
+
+      tagAudio.muted = true;
+    }
+  }
+
   return (
     <Container>
       <button type="button">
-        <img src={audioIcon} alt="audioIcon" />
+        <img className="muted" src={audioMuteIcon} style={{display: `${muted}`}} alt="audioIcon" onClick={e => toogleMute('muted')}/>
+        <img className="notMuted" src={audioIcon} style={{display: `${notMuted}`}} alt="audioIcon" onClick={e => toogleMute('notMuted')}/>
       </button>
 
-      <audio className="tagAudio" src={themeMain} autoPlay loop/>
-      <input className="volume" type="range" min="0" max="100" onMouseMove={e => setVolume(e.target.value)}/>
+      <audio className="tagAudio" src={themeMain} autoPlay loop />
+
+      <input className="volume" type="range"  onMouseMove={e => setVolume(e.currentTarget.value)} />
 
     </Container>
   );
--- src/components/Audio/styles.ts
+++ src/components/Audio/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.div`
 
   display: flex;
   justify-content: center;
+  z-index: 100;
 
   input{
     position: absolute;
